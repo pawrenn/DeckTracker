e233efcf123033d81b78fa2bc69972ba17af129c
pawrenn <38671273+pawrenn@users.noreply.github.com>
2019-05-20 15:23:39 -0500

    some commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'decklist';
+  // loadedFeature = 'decklist';
 
-  onNavigate(feature: string){
-    this.loadedFeature = feature;
-  }
+  // onNavigate(feature: string){
+  //   this.loadedFeature = feature;
+  // }
 }
